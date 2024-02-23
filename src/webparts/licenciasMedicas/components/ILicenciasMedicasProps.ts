8798cc50f2109d0f378a8c734a09db2dd1a03d86
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-02-23 12:20:48 -0400

    initialize project

---
+++ src/webparts/licenciasMedicas/components/ILicenciasMedicasProps.ts
@@ -0,0 +1,7 @@
+export interface ILicenciasMedicasProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/licenciasMedicas/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ILicenciasMedicasWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'LicenciasMedicasWebPartStrings' {
+  const strings: ILicenciasMedicasWebPartStrings;
+  export = strings;
+}
