c735fabdda8a72fdbdae50e7fd483824292f7758
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-02-29 17:57:49 -0400

    fixing styles for the select component

--- src/webparts/licenciasMedicas/components/LicenciasMedicasSelectComponent/LicenciasMedicasSelectComponent.tsx
+++ src/webparts/licenciasMedicas/components/LicenciasMedicasSelectComponent/LicenciasMedicasSelectComponent.tsx
@@ -15,7 +15,7 @@ const LicenciasMedicasSelectComponent: React.FC<ILicenciasMedicasSelectComponent
                 <select
                     id={labelFor}
                     name={labelFor}
-                    className="box-border w-full rounded-sm h-[30px] border-2 border-[#0058a6]  py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    className="box-border w-full rounded-sm h-[30px] border-2 border-[#0058a6] "
                 >
                     <option>United States</option>
                     <option>Canada</option>
