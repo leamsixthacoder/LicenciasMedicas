6b8c130e04eda4b8869170493adb483573eb77ff
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-01 17:37:29 -0400

    fixing some styles and rendering issues

--- src/webparts/licenciasMedicas/components/LicenciasMedicasInputComponent/LicenciasMedicasInputComponent.tsx
+++ src/webparts/licenciasMedicas/components/LicenciasMedicasInputComponent/LicenciasMedicasInputComponent.tsx
@@ -3,18 +3,16 @@ import type { ILicenciasMedicasInputComponentProps } from './ILicenciasMedicasIn
 
 const LicenciasMedicasInputComponent: React.FC<ILicenciasMedicasInputComponentProps> = (props) => {
 
-    const {labelName, inputType, labelFor, isDisabled,} = props
+    const { labelName, inputType, labelFor, isDisabled, } = props
     const disableAttribute = isDisabled ? { disabled: true } : {};
-    const disableStyle = isDisabled ? 'bg-blue-200 border-0' : 'border-2 border-[#0058a6]'
+    const disableStyle = isDisabled ? 'bg-blue-200' : ''
     return (
-        <>
-            <div className='grow'>
-                <label htmlFor={labelFor.toLowerCase()} className='block text-sm font-medium leading-6 text-gray-900'>{labelName}</label>
-                <div className='mt-2'>
-                    <input type={inputType} {...disableAttribute} name={labelFor.toLowerCase()} id={labelFor.toLowerCase()} className={`box-border w-full rounded-sm h-[30px] ${disableStyle} text-sm`} />
-                </div>
+        <div className='grow'>
+            <label htmlFor={labelFor.toLowerCase()} className='block text-sm font-medium leading-6 text-gray-900'>{labelName}</label>
+            <div className='mt-2'>
+                <input type={inputType} {...disableAttribute} name={labelFor.toLowerCase()} id={labelFor.toLowerCase()} className={`box-border w-full rounded-sm h-[30px] focus:border-[#0058a6] border ${disableStyle} text-sm`} />
             </div>
-        </>
+        </div>
     )
 }
 
