df33c0a8b850784794011f3abe062665e0c6a497
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-01 17:37:45 -0400

    using the new components

--- src/webparts/licenciasMedicas/components/LicenciasMedicas.tsx
+++ src/webparts/licenciasMedicas/components/LicenciasMedicas.tsx
@@ -3,6 +3,8 @@ import styles from './LicenciasMedicas.module.scss';
 import type { ILicenciasMedicasProps } from './ILicenciasMedicasProps';
 import LicenciasMedicasInputComponent from './LicenciasMedicasInputComponent/LicenciasMedicasInputComponent'
 import LicenciasMedicasSelectComponent from './LicenciasMedicasSelectComponent/LicenciasMedicasSelectComponent';
+import LicenciasMedicasDatePickerComponent from './LicenciasMedicasDatePickerComponent/LicenciasMedicasDatePickerComponent';
+import LicenciasMedicasButtonComponent from './LicenciasMedicasButtonComponent/LicenciasMedicasButtonComponent';
 // import { escape } from '@microsoft/sp-lodash-subset';
 
 const LicenciasMedicas: React.FC<ILicenciasMedicasProps> = () => {
@@ -29,15 +31,32 @@ const LicenciasMedicas: React.FC<ILicenciasMedicasProps> = () => {
 
         <div className='mt-2 flex justify-between gap-4'>
           <LicenciasMedicasSelectComponent labelName='Colaborador' labelFor='colaborador' />
-          <LicenciasMedicasInputComponent labelName='Posición' labelFor='posicion' isDisabled={true} inputType='text' />
+          <LicenciasMedicasInputComponent labelName='Posición' labelFor='posicion' isDisabled inputType='text' />
         </div>
 
         <div className='mt-2 flex justify-between gap-4'>
-          <LicenciasMedicasInputComponent labelName='Departamento' labelFor='departamento' isDisabled={true} inputType='text' />
-          <LicenciasMedicasInputComponent labelName='Area' labelFor='area' isDisabled={true} inputType='text' />
+          <LicenciasMedicasInputComponent labelName='Departamento' labelFor='departamento' isDisabled inputType='text' />
+          <LicenciasMedicasInputComponent labelName='Area' labelFor='area' isDisabled inputType='text' />
+        </div>
+
+        <div className='mt-2 flex justify-between gap-4'>
+          <LicenciasMedicasInputComponent labelName='Diagnóstico' labelFor='diagnostico' isDisabled={false} inputType='text' />
+          <LicenciasMedicasInputComponent labelName='Rembolso TSS' labelFor='tssrembolso' isDisabled={false} inputType='number' />
+        </div>
+        <div className='mt-2 flex justify-between gap-4'>
+          <LicenciasMedicasDatePickerComponent labelName='Inicio de licencia' placeholder='Seleccionar fecha inicio....' />
+          <LicenciasMedicasDatePickerComponent labelName='Fin de licencia' placeholder='Seleccionar fecha fin....' />
+        </div>
+        <div className='mt-2 flex justify-between gap-4'>
+          <LicenciasMedicasInputComponent labelName='Cant dias' labelFor='cantdias' isDisabled inputType='number' />
+          <LicenciasMedicasInputComponent labelName='Costo licencia' labelFor='costlicencia' isDisabled inputType='number' />
         </div>
 
       </form>
+      <div className='my-4 flex justify-end gap-4'>
+        <LicenciasMedicasButtonComponent buttonName='Guardar' buttonStyle='py-[6px] hover:cursor-pointer border-0 rounded-sm px-5 text-sm bg-sky-700 text-white font-bold' />
+        <LicenciasMedicasButtonComponent buttonName='Limpiar' buttonStyle='py-[6px] hover:cursor-pointer border-0 rounded-sm px-5 text-sm bg-sky-200 text-sky-700 font-bold' />
+      </div>
 
     </>
 
