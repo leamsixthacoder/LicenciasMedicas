10e31191f2abe4d132c6d4ec5ad203192e1bf899
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-06 18:05:59 -0400

    fixing types for employee

--- src/webparts/licenciasMedicas/types/IEmployees.d.ts
+++ src/webparts/licenciasMedicas/types/IEmployees.d.ts
@@ -2,37 +2,9 @@
 export interface Employees {
     Name: string
     EmployeeId: string
-    Email: string
     Area: string
-    BirthDate: string
-    Position: Position
-    Supervisor: Supervisor
-    Manager: Manager
-    Department: Department
+    Position: string
+    Department: string
   }
   
-  export interface Position {
-    PositionId: string
-    DepartmentId: string
-    PositionName: string
-    EmployeeId: string
-    SupervisorPositionId: string
-  }
   
-  export interface Supervisor {
-    SupervisorName: string
-    SupervisorId: string
-    SupervisorEmail: string
-  }
-  
-  export interface Manager {
-    MangerName: string
-    MangerId: string
-    MangerEmail: string
-  }
-  
-  export interface Department {
-    DepartmentName: string
-    ManagerId: string
-    DepartmentId: string
-  }
