3b3177f83cd5c938781ba05d3a9470ddc42e82ac
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-07 17:12:39 -0400

    adding and chaging some styles

--- src/webparts/licenciasMedicas/components/LicenciasMedicasInputComponent/LicenciasMedicasInputComponent.tsx
+++ src/webparts/licenciasMedicas/components/LicenciasMedicasInputComponent/LicenciasMedicasInputComponent.tsx
@@ -5,12 +5,12 @@ const LicenciasMedicasInputComponent: React.FC<ILicenciasMedicasInputComponentPr
 
     const { labelName, inputType, labelFor, isDisabled, value} = props
     const disableAttribute = isDisabled ? { disabled: true } : {};
-    const disableStyle = isDisabled ? 'bg-blue-200' : ''
+    const disableStyle = isDisabled ? 'bg-sky-100' : ''
     return (
         <div className='w-2/4'>
             <label htmlFor={labelFor.toLowerCase()} className='block text-sm font-medium leading-6 text-gray-900'>{labelName}</label>
             <div className='mt-2'>
-                <input type={inputType} {...disableAttribute} value={value} name={labelFor.toLowerCase()} id={labelFor.toLowerCase()} className={`box-border w-full rounded-sm h-[30px] focus:border-[#0058a6] border ${disableStyle} text-sm`} />
+                <input type={inputType} {...disableAttribute} value={value} name={labelFor.toLowerCase()} id={labelFor.toLowerCase()} className={`box-border w-full pl-1 font-semibold text-black rounded-sm h-[30px] focus:border-[#0058a6] border ${disableStyle} text-sm`} />
             </div>
         </div>
     )
