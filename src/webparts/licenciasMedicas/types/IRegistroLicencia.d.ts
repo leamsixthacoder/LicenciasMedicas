257039bb3ab6da33abd1f1c70ec4994c3ce209c7
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-07 17:14:13 -0400

    adding new interface

---
+++ src/webparts/licenciasMedicas/types/IRegistroLicencia.d.ts
@@ -0,0 +1,16 @@
+export interface RegistroLicencia {
+    colaborador: string
+    posicion: string
+    area: string
+    diagnostico: string
+    canthorasdia: number
+    inicioLicencia: string
+    finLicencia: string
+    reingreso: string
+    recibida: string
+    cantdias: number
+    costoLicencia: number
+    rembolsoTss: number
+    comentario: string
+
+}
