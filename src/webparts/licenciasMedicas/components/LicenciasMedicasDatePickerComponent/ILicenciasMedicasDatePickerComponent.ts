910573385d20849a0f12cec2d0c70567df1656db
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-14 17:08:30 -0400

    setting date values

--- src/webparts/licenciasMedicas/components/LicenciasMedicasDatePickerComponent/ILicenciasMedicasDatePickerComponent.ts
+++ src/webparts/licenciasMedicas/components/LicenciasMedicasDatePickerComponent/ILicenciasMedicasDatePickerComponent.ts
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export interface ILicenciasMedicasDatePickerComponent {
     labelName: string,
-    placeholder:string,    
-    value?: any,
+    placeholder:string,   
+    stateName:string, 
+    value?: Date | string,
+    isRequired?: boolean,
+    onChange?: (value: Date | string, inputName: string) => void,
 }
--- src/webparts/licenciasMedicas/components/LicenciasMedicasDatePickerComponent/LicenciasMedicasDatePickerComponent.tsx
+++ src/webparts/licenciasMedicas/components/LicenciasMedicasDatePickerComponent/LicenciasMedicasDatePickerComponent.tsx
@@ -1,14 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as React from 'react'
 import { ILicenciasMedicasDatePickerComponent } from "./ILicenciasMedicasDatePickerComponent"
 import { DatePicker, DayOfWeek } from "office-ui-fabric-react";
 import { DatePickerStrings } from '../../loc/datepickerStrings';
+import * as moment from 'moment';
+import { useState } from 'react';
 
 const LicenciasMedicasDatePickerComponent: React.FC<ILicenciasMedicasDatePickerComponent> = (props) => {
-const {labelName, placeholder} = props;
+    const { labelName, placeholder, stateName, onChange, value, isRequired } = props;
+    const [isValueChanged, setIsValueChanged] = useState(false)
+
+    const handleOnChange = (selectedDate: Date | string) => {
+        setIsValueChanged(true)
+        if (onChange) {
+            onChange(selectedDate, stateName);
+        }
+    }
+
+    const onFormatDate = (date?: Date) => {
+        return moment(date).format("DD/MM/YYYY")
+    }
+    const valueDate = isValueChanged ? moment(value).toDate() : undefined;
     return (
         <div className="w-2/4">
-            <label className='block text-sm font-medium leading-6 text-gray-900'>{labelName}</label>
-            <DatePicker className='box-border w-full rounded-sm h-[30px] text-sm border-2 border-[#0058a6]' firstDayOfWeek={DayOfWeek.Monday} strings={DatePickerStrings} placeholder={placeholder}/>
+            <label className='block text-sm font-medium leading-6 text-gray-900'><strong className='text-red-600'>{isRequired ? '*' : ''}</strong>{' '}{labelName}</label>
+            <DatePicker
+                className='box-border w-full rounded-sm h-[30px] text-sm border-2 border-[#0058a6]'
+                firstDayOfWeek={DayOfWeek.Monday}
+                strings={DatePickerStrings}
+                formatDate={onFormatDate}
+                onSelectDate={(e) => {
+                    if (e)
+                        handleOnChange(
+                            moment(e).toDate()
+                        )
+                    else
+                        handleOnChange(
+                            moment().toDate()
+                        )
+                }}
+
+                placeholder={placeholder}
+                value={valueDate}
+            />
         </div>
 
     )
