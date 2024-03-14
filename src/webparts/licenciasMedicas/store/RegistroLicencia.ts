269b565785e41912dc2d79a02769d9cdd41d9ed9
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-14 17:06:19 -0400

    adding new function to post the registerleave

--- src/webparts/licenciasMedicas/store/RegistroLicencia.ts
+++ src/webparts/licenciasMedicas/store/RegistroLicencia.ts
@@ -6,6 +6,7 @@ interface RegisterState {
     registerLeave: RegistroLicencia,
     setRegisterLeave: (stateName: string, value: any) => void
     setLeaveDays: (amountOfDays: number, payRate: number) => void
+    postRegisterLeave: (registerLeave: RegistroLicencia) => Promise<void>
     setTssRefound: (amount: number) => void
 }
 export const UseRegisterStore = create<RegisterState>()((set, get) => {
@@ -17,22 +18,37 @@ export const UseRegisterStore = create<RegisterState>()((set, get) => {
             Area: '',
             Diagnostic: '',
             TotalHours: null,
-            DepartureDate: '',
-            EndDate: '',
-            ReEntryDate: '',
-            DateRecieved: '',
+            DepartureDate: new Date(),
+            EndDate: new Date(),
+            ReEntryDate: new Date(),
+            DateRecieved: new Date(),
             TotalDays: null,
             LicenseCost: null,
             TSSRefund: null,
             Comments: '',
+            Date: new Date(),
+            User: ''
         },
-        setRegisterLeave: (stateName: string, value: any) =>{
-            const { registerLeave } = get()
-            const updatedLeave = { ...registerLeave, [stateName]: value };
-            set({ registerLeave: updatedLeave })
-            // console.log(registerLeave)
+        setRegisterLeave: (stateName: string, value: any) => {
+            const { registerLeave } = get();
+            let updatedValue: any;
+        
+            // Convert valid numeric strings to numbers
+            if (stateName === 'TotalDays' || stateName === 'TSSRefund' || stateName === 'TotalHours') {
+                if (typeof value === 'string' && !isNaN(parseFloat(value))) {
+                    updatedValue = parseFloat(value);
+                } else {
+                    updatedValue = null; // Or handle invalid values appropriately
+                }
+            } else {
+                updatedValue = value;
+            }
+        
+            const updatedLeave = { ...registerLeave, [stateName]: updatedValue };
+            set({ registerLeave: updatedLeave });
         },
-        setLeaveDays: (amountOfDays: number, payRate: number) =>{
+        
+        setLeaveDays: (amountOfDays: number, payRate: number) => {
             const calculateCost = (payRate / 23.83) * amountOfDays
             const { registerLeave } = get()
             const updatedCost = Math.round(calculateCost);
@@ -40,7 +56,7 @@ export const UseRegisterStore = create<RegisterState>()((set, get) => {
             set({ registerLeave: updatedLeave })
 
         },
-        setTssRefound: (amount: number) =>{
+        setTssRefound: (amount: number) => {
             const { registerLeave } = get()
             const costLeave = registerLeave.LicenseCost
             if (costLeave !== null) {
@@ -49,6 +65,28 @@ export const UseRegisterStore = create<RegisterState>()((set, get) => {
                 set({ registerLeave: updatedLeave })
             }
 
+        },
+
+        postRegisterLeave: async (registerLeave: RegistroLicencia) => {
+            try {
+                const response = await fetch('https://localhost:5001/api/medical-leave/MedicalLeave', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(registerLeave)
+                });
+
+                if (!response.ok) {
+                    throw new Error('Algo anda mal')
+                }
+
+                const responseData = await response.json();
+                console.log('Guardado exitosamente')
+                return responseData
+            } catch (error) {
+                console.log('Error')
+            }
         }
 
     }
