6a5ca8fee8a1575b79b69fcd23bdc920c0ab643a
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-14 17:59:41 -0400

    starting the validation function

--- src/webparts/licenciasMedicas/types/IValidation.d.ts
+++ src/webparts/licenciasMedicas/types/IValidation.d.ts
@@ -4,8 +4,10 @@ export const getValidation = (registerLeave: RegistroLicencia) => {
 
     let errors = {};
     
-    if(!registerLeave.colaborador) errors['colaborador'] = 'Favor seleccionar un colaborador'
-
+    if(!registerLeave.Name) errors['Name'] = 'Favor seleccionar un colaborador'
+    if(!registerLeave.Diagnostic) errors['Diagnostic'] = 'Favor introducir diagnostico'
+    if(!registerLeave.DepartureDate) errors['DepartureDate'] = 'Favor seleccionar la fecha de inicio'
+    if(!registerLeave.EndDate) errors['EndDate'] = 'Favor seleccionar la fecha de fin'
     return errors
 
 }
