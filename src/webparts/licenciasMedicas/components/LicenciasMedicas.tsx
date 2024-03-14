ece37a656abece91bf76ea8e9f7112f16fbbf514
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-14 17:09:18 -0400

    adding new functions from the state handler

--- src/webparts/licenciasMedicas/components/LicenciasMedicas.tsx
+++ src/webparts/licenciasMedicas/components/LicenciasMedicas.tsx
@@ -10,8 +10,6 @@ import LicenciasMedicasTextareaComponent from './LicenciasMedicasTextareaCompone
 import { UseEmployeesStore } from '../store/employee';
 import { useEffect } from 'react';
 import { UseRegisterStore } from '../store/RegistroLicencia';
-// import { escape } from '@microsoft/sp-lodash-subset';
-
 const LicenciasMedicas: React.FC<ILicenciasMedicasProps> = () => {
 
   const employees = UseEmployeesStore(state => state.employees)
@@ -23,6 +21,7 @@ const LicenciasMedicas: React.FC<ILicenciasMedicasProps> = () => {
   const setRegisterLeave = UseRegisterStore((state => state.setRegisterLeave))
   const setLeaveDays = UseRegisterStore((state => state.setLeaveDays))
   const setTssRefound = UseRegisterStore((state => state.setTssRefound))
+  const postRegisterLeave = UseRegisterStore((state => state.postRegisterLeave))
 
   useEffect(() => {
     void fetchEmployees();
@@ -37,26 +36,42 @@ const LicenciasMedicas: React.FC<ILicenciasMedicasProps> = () => {
     return options
   }
 
-  const handleSelectEmployee = (selectedOption: any, stateName: string) => {
+  const handleSelectEmployee = (selectedOption: any) => {
 
     const selectedEmployeeId = selectedOption.value;
-       selectEmployee(selectedEmployeeId)
-       const selectedEmployeeName = selectedOption.label.split(' ').slice(1).join(' ')
-       setRegisterLeave(stateName, selectedEmployeeName)
-       setRegisterLeave('Code', selectedEmployeeId)
-  };
-
-  const handleInputChange = (value: any, inputName: string) => {
-    console.log('Input Name:', inputName);
-    console.log('Input Value:', value);
-    if (selectedEmployee) {
-      if (inputName === 'TotalDays') setLeaveDays(value, selectedEmployee.Salary)
-      if (inputName === 'TSSRefund') setTssRefound(value)
-      if (inputName === 'Position') console.log('hola')
-       setRegisterLeave(inputName, value)
+    selectEmployee(selectedEmployeeId)
+    const selectedEmployeeProps = employees.find(employee => employee.EmployeeId === selectedEmployeeId)
+    if (selectedEmployeeProps) {
+      setRegisterLeave('Name', selectedEmployeeProps.Name)
+      setRegisterLeave('Code', selectedEmployeeProps.EmployeeId)
+      setRegisterLeave('Position', selectedEmployeeProps.Position)
+      setRegisterLeave('Area', selectedEmployeeProps.Area)
     }
 
   };
+
+  const handleInputChange = (value: any, inputName: string) => {
+    if (selectedEmployee) {
+      if (inputName === 'TotalDays') setLeaveDays(value, selectedEmployee.Salary)
+      if (inputName === 'TSSRefund') setTssRefound(value)
+      setRegisterLeave(inputName, value)
+    }
+  };
+  // const convertDateFormat = (dateString: string | Date) => {
+  //   const formattedDate = moment(dateString, 'YYYY/MM/DD').format('YYYY-MM-DD');
+  //   return formattedDate;
+  // }
+
+  const sendRegisterLeave = async () => {
+
+    setRegisterLeave('DepartureDate', registerLeave.DepartureDate)
+    setRegisterLeave('EndDate', registerLeave.EndDate)
+    setRegisterLeave('ReEntryDate', registerLeave.ReEntryDate)
+    setRegisterLeave('DateRecieved', registerLeave.DateRecieved)
+    setRegisterLeave('Date', registerLeave.Date)
+    console.log(typeof(registerLeave.DepartureDate))
+     await postRegisterLeave(registerLeave)
+  }
 
   return (
 
@@ -71,7 +86,7 @@ const LicenciasMedicas: React.FC<ILicenciasMedicasProps> = () => {
       <form className='mt-10 mx-2'>
 
         <div className='mt-2 flex justify-between gap-4'>
-          <LicenciasMedicasSelectComponent labelName='Colaborador' labelFor='Name' options={getEmployeeOptions()} isLoading={isLoading} onSelect={handleSelectEmployee} />
+          <LicenciasMedicasSelectComponent labelName='Colaborador' isRequired labelFor='Name' options={getEmployeeOptions()} isLoading={isLoading} onSelect={handleSelectEmployee} />
           <LicenciasMedicasInputComponent labelName='Posición' labelFor='Position' onChange={handleInputChange} isDisabled inputType='text' value={selectedEmployee?.Position} />
         </div>
 
@@ -82,15 +97,15 @@ const LicenciasMedicas: React.FC<ILicenciasMedicasProps> = () => {
 
         <div className='mt-2 flex justify-between gap-4'>
           <LicenciasMedicasInputComponent labelName='Diagnóstico' isRequired labelFor='Diagnostic' isDisabled={false} inputType='text' onChange={handleInputChange} value={registerLeave.Diagnostic} />
-          <LicenciasMedicasInputComponent labelName='Horas diarias de trabajo' labelFor='TotalHours' isDisabled={false} inputType='number' onChange={handleInputChange} value={registerLeave.TotalHours}/>
+          <LicenciasMedicasInputComponent labelName='Horas diarias de trabajo' labelFor='TotalHours' isDisabled={false} inputType='number' onChange={handleInputChange} value={registerLeave.TotalHours} />
         </div>
         <div className='mt-2 flex justify-between gap-4'>
-          <LicenciasMedicasDatePickerComponent labelName='Inicio de licencia' placeholder='Seleccionar fecha inicio....' />
-          <LicenciasMedicasDatePickerComponent labelName='Fin de licencia' placeholder='Seleccionar fecha fin....' />
+          <LicenciasMedicasDatePickerComponent labelName='Inicio de licencia' isRequired stateName='DepartureDate' onChange={handleInputChange} placeholder='Seleccionar fecha inicio....' value={registerLeave.DepartureDate} />
+          <LicenciasMedicasDatePickerComponent labelName='Fin de licencia' isRequired  stateName='EndDate' onChange={handleInputChange} placeholder='Seleccionar fecha fin....' value={registerLeave.EndDate} />
         </div>
         <div className='mt-2 flex justify-between gap-4'>
-          <LicenciasMedicasDatePickerComponent labelName='Reingreso' placeholder='Seleccionar fecha Reingreso....' />
-          <LicenciasMedicasDatePickerComponent labelName='Recibida' placeholder='Seleccionar fecha Recibida....' />
+          <LicenciasMedicasDatePickerComponent labelName='Reingreso' stateName='ReEntryDate' onChange={handleInputChange} placeholder='Seleccionar fecha Reingreso....' value={registerLeave.ReEntryDate} />
+          <LicenciasMedicasDatePickerComponent labelName='Recibida' stateName='DateRecieved' onChange={handleInputChange} placeholder='Seleccionar fecha Recibida....' value={registerLeave.DateRecieved} />
         </div>
         <div className='mt-2 flex justify-between gap-4'>
           <LicenciasMedicasInputComponent labelName='Cant dias' labelFor='TotalDays' isDisabled={false} inputType='number' onChange={handleInputChange} value={registerLeave.TotalDays} />
@@ -101,13 +116,13 @@ const LicenciasMedicas: React.FC<ILicenciasMedicasProps> = () => {
 
         </div>
         <div className='mt-2 flex justify-start'>
-          <LicenciasMedicasTextareaComponent labelName='Comentario' labelFor='comentario' value={registerLeave.Comments}/>
+          <LicenciasMedicasTextareaComponent labelName='Comentario' labelFor='Comments' value={registerLeave.Comments} onChange={handleInputChange} />
         </div>
 
       </form>
       <div className='my-4 flex justify-end gap-4'>
-        <LicenciasMedicasButtonComponent buttonName='Guardar' buttonStyle='py-[6px] hover:cursor-pointer border-0 rounded-sm px-5 text-sm bg-sky-700 text-white font-bold' />
-        <LicenciasMedicasButtonComponent buttonName='Limpiar' buttonStyle='py-[6px] hover:cursor-pointer border-0 rounded-sm px-5 text-sm bg-sky-200 text-sky-700 font-bold' />
+        <LicenciasMedicasButtonComponent onClick={sendRegisterLeave} buttonName='Guardar' buttonStyle='py-[6px] hover:cursor-pointer border-0 rounded-sm px-5 text-sm bg-sky-700 text-white font-bold' />
+        <LicenciasMedicasButtonComponent onClick={sendRegisterLeave} buttonName='Limpiar' buttonStyle='py-[6px] hover:cursor-pointer border-0 rounded-sm px-5 text-sm bg-sky-200 text-sky-700 font-bold' />
       </div>
 
     </>
