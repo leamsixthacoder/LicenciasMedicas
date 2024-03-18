efbe40a6d45fc50cd9db1ae62a2d17d443363ef8
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-18 17:59:42 -0400

    adding props to reset select value

--- src/webparts/licenciasMedicas/components/LicenciasMedicasSelectComponent/ILicenciasMedicasSelectComponentProps.ts
+++ src/webparts/licenciasMedicas/components/LicenciasMedicasSelectComponent/ILicenciasMedicasSelectComponentProps.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+
 export interface ILicenciasMedicasSelectComponentProps {
     labelName: string,
     labelFor: string,
     options: any[],
     isLoading: boolean,
     isRequired?: boolean,
+    selectedValue:any
+    setSelectedValue:any
     onSelect: (selectedOption: any) => void
 }
