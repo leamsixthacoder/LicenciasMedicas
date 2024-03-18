076b6420c61f976ab7dafab4967ea9a1a67bb168
Ismael Vasquez <71105538+leamsi-ciber@users.noreply.github.com>
2024-03-18 17:58:23 -0400

    adding initial default values and reset state function

--- src/webparts/licenciasMedicas/store/RegistroLicencia.ts
+++ src/webparts/licenciasMedicas/store/RegistroLicencia.ts
@@ -5,33 +5,35 @@ import { type RegistroLicencia } from "../types/IRegistroLicencia";
 import { calculateCost } from "../logic/CalculateCost";
 import { RegisterLeaveService } from "../services/RegisterLeaveService";
 
+const initialRegisterLeave: RegistroLicencia = {
+    Code: '',
+    Name: '',
+    Position: '',
+    Area: '',
+    Diagnostic: '',
+    TotalHours: 0,
+    DepartureDate: new Date(),
+    EndDate: new Date(),
+    ReEntryDate: new Date(),
+    DateRecieved: new Date(),
+    TotalDays: 0,
+    LicenseCost: 0,
+    TSSRefund: 0,
+    Comments: '',
+    Date: new Date(),
+    User: ''
+};
 export interface RegisterState {
     registerLeave: RegistroLicencia,
     setRegisterLeave: (stateName: string, value: any, payRate?: number) => void
     setLeaveTotalCost: (leaveTotalCost: number) => void
     postRegisterLeave: (registerLeave: RegistroLicencia) => Promise<void>
     calculateCost: (totalDays: number | null, salary: number, tssRefound: number | null) => void
+    resetRegisterLeaveState: () => void
 }
 export const UseRegisterStore = create<RegisterState>()((set, get) => {
     return {
-        registerLeave: {
-            Code: '',
-            Name: '',
-            Position: '',
-            Area: '',
-            Diagnostic: '',
-            TotalHours: null,
-            DepartureDate: new Date(),
-            EndDate: new Date(),
-            ReEntryDate: new Date(),
-            DateRecieved: new Date(),
-            TotalDays: null,
-            LicenseCost: null,
-            TSSRefund: null,
-            Comments: '',
-            Date: new Date(),
-            User: ''
-        },
+        registerLeave: initialRegisterLeave,
         setRegisterLeave: (stateName: string, value: any, payRate?: number) => {
             const { registerLeave, setLeaveTotalCost } = get();
             let updatedValue: any
@@ -70,6 +72,10 @@ export const UseRegisterStore = create<RegisterState>()((set, get) => {
             await RegisterLeaveService.postRegisterLeave(registerLeave)
         },
         calculateCost: calculateCost,
+        resetRegisterLeaveState: () => {
+            const registerLeave = initialRegisterLeave
+            set({registerLeave})
+        }
 
     }
 })
